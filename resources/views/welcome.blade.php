631083fb2aaf84d5b7a7d71f5ca903811c64d475
muneeb <muneeb.rehman@rltsquare.com>
2022-07-31 21:29:26 +0500

    updated.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,31 @@
 @section('content')
     <!-- You are: (shop domain name) -->
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+    <h6>All Customers</h6>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Full Name</th>
+            <th scope="col">Comapny</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Address</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($allCustomers as $customer)   
+            <tr>
+              <th scope="row">{{$customer->id}}</th>
+              <td>{{$customer->first_name . " " . $customer->last_name}}</td>
+              <td>{{$customer->Comapny}}</td>
+              <td>{{$customer->email}}</td>
+              <td>{{$customer->phone}}</td>
+              <td>{{$customer->street_address . $customer->street_address_2 . ', \n' . $customer->city . ', ' . $customer->state . ', ' . $customer->zip_code}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+      </table>
 @endsection
 
 @section('scripts')
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['verify.shopify'])->group(function () {
-    Route::get('/', 'App\Http\Controllers\CustomersController@appDashboard')->name('home');
+    Route::get('/', 'App\Http\Controllers\CustomerController@appDashboard')->name('home');
 });
 
 // Route::get('/', function () {
